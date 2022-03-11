595ddfc4ea7ef1afe847e74014455cf9d9605aa2
Daniel Poborchiy <danil31work@gmail.com>
2022-03-11 20:27:20 +0200

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Note.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace PhoneBook
+{
+    // Класс запись
+    class Note
+    {
+        public int PhoneNumber;
+        public string Surname;
+        public DateTime Birthday;
+
+
+        public Note()       // конструктор по умолчанию
+        {
+        }
+
+
+        public Note(int _PhoneNumber, string _Surname, DateTime _Birthday)   // Конструктор класса
+        {
+            PhoneNumber = _PhoneNumber;
+            Surname = _Surname;
+            Birthday = _Birthday;
+
+        }
+
+
+        // функция вывода на экрам записной книги
+        public static void Display(Note[] MyNote)
+        {
+            if (MyNote.Length != 0)
+                for (int i = 0; i < MyNote.Length; i++)
+                    Console.WriteLine("{3} : Фамилия {0} , Дата рождения {1} , Номер телефона {2}", MyNote[i].Surname, MyNote[i].Birthday, MyNote[i].PhoneNumber, i + 1);
+            else
+                Console.WriteLine("Записная книга пуста");
+        }
+
+
+
+
+        public static void NumberSort(Note[] MyNote)  // сортировка по номеру
+        {
+            Array.Sort(MyNote, LogicSortNumber);
+        }
+
+
+        public static void BirthdaySort(Note[] MyNote)  // сортировка по дате рождения
+        {
+            Array.Sort(MyNote, LogicSortBirthday);
+        }
+
+        public static void SurNameSort(Note[] MyNote)// сортировка по фамилии
+        {
+            Array.Sort(MyNote, LogicSortSurName);
+        }
+
+
+
+        static int LogicSortNumber(Note x, Note y)
+        {
+            if (x.PhoneNumber < y.PhoneNumber) return -1; //порядок соблюден
+            if (x.PhoneNumber > y.PhoneNumber) return +1; //порядок нарушен
+            return 0; // безразличие
+
+        }
+
+        static int LogicSortBirthday(Note x, Note y)
+        {
+            if (x.Birthday < y.Birthday) return -1; //порядок соблюден
+            if (x.Birthday > y.Birthday) return +1; //порядок нарушен
+            return 0; // безразличие
+        }
+
+
+
+        static int LogicSortSurName(Note x, Note y)
+        {
+            int i = 0;
+
+            while (i < x.Surname.Length && i < y.Surname.Length)
+            {
+
+                if (x.Surname[i] < y.Surname[i]) return -1; //порядок соблюден
+                if (x.Surname[i] > y.Surname[i]) return +1; //порядок нарушен
+                i++;
+            }
+            if (x.Surname.Length < y.Surname.Length) return -1; //порядок соблюден
+            if (x.Surname.Length > y.Surname.Length) return +1; //порядок нарушен
+            return 0;
+
+
+        }
+
+
+
+        // Поиск по номеру или фамилии 
+        public static void SearchNote(Note[] MyNote, string search)
+        {
+            int number = 0;
+            bool flag = int.TryParse(search, out number);
+
+            for (int i = 0; i < MyNote.Length; i++)
+            {
+
+                if (flag)
+                    if (MyNote[i].PhoneNumber == int.Parse(search)) // Поиск по номеру
+                    {
+                        Console.WriteLine("Фамилия {0} , Дата рождения {1} , Номер телефона {2}", MyNote[i].Surname, MyNote[i].Birthday, MyNote[i].PhoneNumber);
+                        number++;
+                    }
+                if (!flag)
+                    if (MyNote[i].Surname == search) // Поиск по имени
+                    {
+                        Console.WriteLine("Фамилия {0} , Дата рождения {1} , Номер телефона {2}", MyNote[i].Surname, MyNote[i].Birthday, MyNote[i].PhoneNumber);
+                        number++;
+                    }
+            }
+
+            if (number == 0) // Если пустая книга
+                Console.WriteLine("Нет записей с искомыми параметрами");
+
+
+
+        }
+
+
+        // удаление из записной книжки 
+        public static Note[] DeleteNote(Note[] MyNote, int numberNote)
+        {
+            Note[] temp = new Note[MyNote.Length - 1];
+            int k = 0;
+
+            for (int i = 0; i < MyNote.Length; i++)
+            {
+                if (i == numberNote)
+                    continue;
+                temp[k++] = MyNote[i];
+
+            }
+
+            return temp;
+
+        }
+
+    }
+
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneBook
+{
+    internal class Program
+    {
+        delegate void method();
+        static void Main(string[] args)
+        {
+            Note[] TheMyNote = new Note[0];
+            DateTime DT = new DateTime();
+            int Year, Mounth, Day;
+
+            string[] items = { "0.Заполнить для наглядности",
+                               "1.Добавить Новую запись",
+                               "2.Отобразить записную книжку",
+                               "3.Отсортироовать по номеру",
+                               "4.Отсортироовать по Дате рождения",
+                               "5.Отсортироовать по фамилии",
+                               "6.Поиск",
+                               "7.Удалить запись",
+                               "------------------------------------" };
+            method[] methods = new method[] { Method0, Method1, Method2, Method3, Method4, Method5, Method6, Method7 };
+            ConsoleMenu menu = new ConsoleMenu(items);
+            int menuResult;
+            do
+            {
+                menuResult = menu.PrintMenu();
+                methods[menuResult]();
+                Console.WriteLine("Для продолжения нажмите любую клавишу");
+                Console.ReadKey();
+            } while (menuResult != items.Length - 1);
+
+            void Method0()
+            {
+                Console.WriteLine("Выбрана опция 0");
+
+                Note[] temp1 = new Note[5];
+                temp1[0] = new Note(129451, "Герберт", new DateTime(1991, 4, 1));
+                temp1[1] = new Note(123451, "Пушкин", new DateTime(1990, 6, 4));
+                temp1[2] = new Note(658754, "Глуховский", new DateTime(1994, 2, 10));
+                temp1[3] = new Note(798456, "Роулинг", new DateTime(1999, 9, 11));
+                temp1[4] = new Note(751668, "Сапковский", new DateTime(1998, 2, 6));
+
+                TheMyNote = temp1;
+
+            }
+            void Method1()
+            {
+                Console.WriteLine("Выбрана опция 1");
+
+                Note[] temp = new Note[TheMyNote.Length + 1];
+
+                temp[temp.Length - 1] = new Note();
+
+                for (int i = 0; i < TheMyNote.Length; i++)
+                    temp[i] = TheMyNote[i];
+                Console.WriteLine("Введите номер телефона");
+                int.TryParse(Console.ReadLine(), out temp[temp.Length - 1].PhoneNumber);
+                Console.WriteLine("Введите фамилию");
+                temp[temp.Length - 1].Surname = Console.ReadLine();
+                Console.WriteLine("Введите год рождения");
+                Year = int.Parse(Console.ReadLine());
+                Console.WriteLine("Введите месяц рождения");
+                Mounth = int.Parse(Console.ReadLine());
+                Console.WriteLine("День");
+                Day = int.Parse(Console.ReadLine());
+                DT = new DateTime(Year, Mounth, Day);
+
+                temp[temp.Length - 1].Birthday = DT;
+
+                TheMyNote = temp;
+            }
+            void Method2()
+            {
+                Console.WriteLine("Выбрана опция 2");
+
+                Note.Display(TheMyNote);
+            }
+            void Method3()
+            {
+                Console.WriteLine("Выбрана опция 3");
+
+                Note.NumberSort(TheMyNote);
+                Note.Display(TheMyNote);
+            }
+            void Method4()
+            {
+                Console.WriteLine("Выбрана опция 4");
+
+                Note.BirthdaySort(TheMyNote);
+                Note.Display(TheMyNote);
+            }
+            void Method5()
+            {
+                Console.WriteLine("Выбрана опция 5");
+
+                Note.SurNameSort(TheMyNote);
+                Note.Display(TheMyNote);
+            }
+            void Method6()
+            {
+                Console.WriteLine("Выбрана опция 6");
+
+                Console.WriteLine("Введите фимилию , или номер телефона");
+                Note.SearchNote(TheMyNote, Console.ReadLine());
+            }
+            void Method7()
+            {
+                Console.WriteLine("Выбрана опция 7");
+
+                Note.Display(TheMyNote);
+                Console.WriteLine("Введите номер записи для удаления");
+                int num = -1;
+                int.TryParse(Console.ReadLine(), out num);
+
+                if (num > 0 && num < TheMyNote.Length - 1)
+                {
+                    TheMyNote = Note.DeleteNote(TheMyNote, num - 1);
+                }
+
+                Note.Display(TheMyNote);
+            }
+
+        }
+    }
+}
